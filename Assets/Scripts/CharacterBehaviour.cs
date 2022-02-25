8420dddbc8311addb5f4d9ded8de5de022140be4
Kharalos <ounal97@gmail.com>
2022-02-25 09:43:12 +0300

    Karakter

--- Assets/Scripts/CharacterBehaviour.cs
+++ Assets/Scripts/CharacterBehaviour.cs
@@ -6,6 +6,11 @@ using UnityEngine;
 public class CharacterBehaviour : MonoBehaviour
 {
     private Animator m_animator;
+    private CharacterController m_controller;
+    float timer;
+    Quaternion targetRot;
+    Vector3 swerveDirection, targetSwerveDirection;
+    [SerializeField] private float platformWidth = 4f;
 
     [SerializeField] private bool running;
     [SerializeField] private float runSpeed = 3, swerveSpeed = 2;
@@ -13,27 +18,39 @@ public class CharacterBehaviour : MonoBehaviour
 
     void Start()
     {
-        //m_animator = GetComponent<Animator>();
+        timer = 0.0f;
+        m_animator = GetComponent<Animator>();
+        m_controller = GetComponent<CharacterController>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
+        if (SwerveInput.swerveLeft && transform.position.x > -platformWidth)
+        {
+            targetSwerveDirection = Vector3.left;
+            targetRot = Quaternion.Euler(0, -25f, 0);
+        }
+        else if (SwerveInput.swerveRight && transform.position.x < platformWidth)
+        {
+            targetSwerveDirection = Vector3.right;
+            targetRot = Quaternion.Euler(0, 25f, 0);
+        }
+        else
+        {
+            targetSwerveDirection = Vector3.zero;
+            targetRot = Quaternion.identity;
+        }
+        swerveDirection = Vector3.Lerp(swerveDirection, targetSwerveDirection, 2f * Time.deltaTime);
+
         if (running)
         {
-            //m_rigidbody.velocity = Vector3.forward * Time.deltaTime * speed;
-            transform.position += Vector3.forward * Time.deltaTime * runSpeed;
+            m_controller.Move((Vector3.forward * Time.deltaTime * runSpeed) + (swerveDirection * Time.deltaTime * swerveSpeed));
         }
-        if (SwerveInput.swerveLeft)
-        {
-            transform.position += Vector3.left * Time.deltaTime * swerveSpeed;
-        }
-        else if (SwerveInput.swerveRight)
-        {
-            transform.position += Vector3.right * Time.deltaTime * swerveSpeed;
-        }
-        var pos = transform.position;
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, 4f * Time.deltaTime);
+
+        /*var pos = transform.position;
         pos.x = Mathf.Clamp(transform.position.x, -4.0f, 4.0f);
-        transform.position = pos;
+        transform.position = pos;*/
 
     }
     public void StartRun()
