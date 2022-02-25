6946b943b208b01bac4dfe06b1c87b4bdbafd49c
Kharalos <ounal97@gmail.com>
2022-02-25 13:09:46 +0300

    Money Animation

--- Assets/Scripts/CollectHandler.cs
+++ Assets/Scripts/CollectHandler.cs
@@ -1,17 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class CollectHandler : MonoBehaviour
 {
-/// <summary>
-/// OnTriggerEnter is called when the Collider other enters the trigger.
-/// </summary>
-/// <param name="other">The other Collider involved in this collision.</param>
-void OnTriggerEnter(Collider other)
-{
-    if(other.CompareTag("Player")){
-        Debug.Log("Collided with money");
+    [SerializeField] private float animDuration = 0.5f;
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player")){
+            Debug.Log("Collided with money");
+            StartCoroutine(Collected());
+        }
     }
-}
+    IEnumerator Collected(){
+        // Create a new Sequence.
+		Sequence s = DOTween.Sequence();
+		// Add an horizontal relative move tween that will last the whole Sequence's duration
+		s.Append(transform.DOScale(1.1f, animDuration).SetRelative());
+		// Insert a rotation tween which will last half the duration
+		s.Insert(animDuration, transform.DOScale(0.0f, animDuration / 2));
+
+        yield return new WaitForSeconds(animDuration * 1.5f);
+        
+        Destroy(gameObject);
+    }
 }
